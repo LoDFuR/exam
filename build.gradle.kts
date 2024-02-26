28e20a402e52a4eaf29a0d0d4deaa219a09e3885
LoDFuR <LoDFuR1@gmail.com>
2024-02-27 00:22:02 +0300

    working?!

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ testImplementation("org.assertj:assertj-core:3.25.1")
 
   constraints {
         api("org.junit:junit-bom:5.10.1")
-        api("org.assertj:assertj-core:3.25.1")
+        //api("org.assertj:assertj-core:3.25.1")
     }
 	//testImplementation("org.junit:junit-bom:5.10.1")
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
